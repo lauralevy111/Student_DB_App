07a6c482d56b4c8712a240159b80fa69f09d1e81
lauralevy510 <llevy510@gmail.com>
2021-02-23 08:29:00 -0500

    added some s.o.p's to constructor w scanner in

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
@@ -12,7 +14,18 @@ public class Student {
 
     //constructor: prom user to enter student's name + year
     public Student(){
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter Student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextLine();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+
     }
 
     //TODO: generate an id
