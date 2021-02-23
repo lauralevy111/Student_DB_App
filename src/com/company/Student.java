b302ef1136d25ce860fa5145b6792a7661fc1850
lauralevy510 <llevy510@gmail.com>
2021-02-23 08:19:08 -0500

    added todo items to student. javaa

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -2,13 +2,6 @@ package com.company;
 
 public class Student {
 
-    private String firstName;
-    private String lastName;
-    private String gradeYear;
-    private int studentID;
-    private String courses;
-    private String tuitionBalance;
-    private int costOfCourse;
 
     //TODO: constructor: prom uter to enter student's name + year
 
