44886faae5c4500615041c17befba9bb750365bb
lauralevy510 <llevy510@gmail.com>
2021-02-23 08:31:29 -0500

    created instance of student in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Student student1 = new Student();
         //TODO: Ask how many users we want to add
 
         //TODO: create n number of new studnts
