b41570f1b0048885911b447816276982d291a10f
lauralevy510 <llevy510@gmail.com>
2021-02-23 08:20:15 -0500

    added todo items to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //TODO: Ask how many users we want to add
+
+        //TODO: create n number of new studnts
     }
 }
