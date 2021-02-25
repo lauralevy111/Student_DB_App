faae88a8eced796e10fcc98bd5d74fd4a889c199
lauralevy510 <llevy510@gmail.com>
2021-02-25 09:53:11 -0500

    removed args from call of payTuition() in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Student student1 = new Student();
         student1.enroll();
-        student1.payTuition(500);
+        student1.payTuition();
         //TODO: Ask how many users we want to add
 
         //TODO: create n number of new studnts
