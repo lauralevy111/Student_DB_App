d9bec829bcc0768417e3f73e6872d8105c3c7bb0
lauralevy510 <llevy510@gmail.com>
2021-02-25 10:01:48 -0500

    created showStatus method that returns a string of data about the student

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -76,6 +76,12 @@ public class Student {
         viewBalance();
     }
 
-    //TODO: show status
+    //show status
+    public String showStatus() {
+        String status= "name: "+firstName+" " + lastName +
+                "\nCourses Enrolled: " + courses +
+                "\nBalance: $" + tuitionBalance;
+        return status;
+    }
 
 }
