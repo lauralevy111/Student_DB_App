00cae90c4302aa46db8de4bf9090aa326b6e2b93
lauralevy510 <llevy510@gmail.com>
2021-02-26 08:54:13 -0500

    removed instances

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,6 @@ public class Main {
         student1.payTuition();
         student1.showStatus();
 
-        Student student2 = new Student();
-        student2.enroll();
-        student2.payTuition();
-        student2.showStatus();
-
-        Student student3 = new Student();
-        student3.enroll();
-        student3.payTuition();
-        student3.showStatus();
-
         //TODO: Ask how many users we want to add
 
         //TODO: create n number of new studnts
