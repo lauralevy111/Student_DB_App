25ba1d3c3cea90efeb1fa8e8c3d097bc04adba10
lauralevy510 <llevy510@gmail.com>
2021-02-26 09:05:37 -0500

    commented out the student obj created b4 we ask for # of students

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        /*
         Student student1 = new Student();
         student1.enroll();
         student1.payTuition();
         student1.showStatus();
+        
+         */
 
         //Ask how many users we want to add
         System.out.print("Enter nubmer of students to enroll: ");
